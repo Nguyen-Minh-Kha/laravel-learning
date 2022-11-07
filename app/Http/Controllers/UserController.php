48a6189dc0cdf34a6c0deaa280bbef0ef1b873ac
@Nguyen-Minh-Kha <pro.nguyenminhkha@gmail.com>
2022-11-07 16:56:24 +0100

    ch 6 créer un controlleur et relier à sa route

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function profile(string $username)
+    {
+        return 'I am an user and my name is '.$username;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\Routing\Annotation\Route as AnnotationRoute;
 
+//use the controller
+use App\Http\Controllers\{
+    UserController
+};
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,84 +25,106 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*
+|--------------------------------------------------------------------------
+| ch 6
+|--------------------------------------------------------------------------
+*/
+
+Route::get('profile/{username}', [UserController::class, 'profile'])->name('user.profile');
+
+
+
+/*
+|--------------------------------------------------------------------------
+| ch 5
+|--------------------------------------------------------------------------
+*/
+
 //retourner une view
 // Route::get('test', function (){
 //     return view('test.index');
 // });
 
-//passer les params URL à la view
-
-Route::get('profile/{firstname}/{lastname}', function($firstname = null, $lastname = null){
+// //passer les params URL à la view
+// Route::get('profile/{firstname}/{lastname}', function($firstname = null, $lastname = null){
     
-    //return view('profile.index')-> with('firstname', $firstname)-> with ('lastname', $lastname);
+//     //return view('profile.index')-> with('firstname', $firstname)-> with ('lastname', $lastname);
 
-    //return view('profile.index')->with(compact('firstname', 'lastname'));
+//     //return view('profile.index')->with(compact('firstname', 'lastname'));
 
-    //return view('profile.index')->withFirstname($firstname)->withLastname($lastname);
+//     //return view('profile.index')->withFirstname($firstname)->withLastname($lastname);
 
-    $data = [
-        'title' => 'my title',
-        'description' => 'my description',
-        'firstname' => $firstname,
-        'lastname' => $lastname,
-    ];
+//     $data = [
+//         'title' => 'my title',
+//         'description' => 'my description',
+//         'firstname' => $firstname,
+//         'lastname' => $lastname,
+//     ];
 
-    return view('profile.index',$data);
-});
-
-
-//passer les query string à la view 
-
-Route::get('test' ,function(){
-    $firstname = request()->query('firstname', null);
-    $lastname = request()->query('lastname', null);
-    $data = [
-        'title' => "Page de ".$firstname,
-        'description' => "Page de ".$firstname .' '. $lastname,
-        'firstname' => $firstname,
-        'lastname' => $lastname,
-    ];
-
-    return view('test.index', $data);
-});
-
-
-Route::get("test/{user}", function ($user) {
-    echo 'hello ' . $user;
-});
-
-// Route::get('profile/{name?}', function ($name = null) {
-//     if ($name) {
-//         echo 'hello ' . $name;
-//     } else {
-//         echo 'hello stranger';
-//     }
+//     return view('profile.index',$data);
 // });
 
-Route::get('articles', function () {
-    $articles = ['B', 'A', 'C'];
 
-    $sort = request()->query('sort', 'desc');
+// //passer les query string à la view 
+// Route::get('test' ,function(){
+//     $firstname = request()->query('firstname', null);
+//     $lastname = request()->query('lastname', null);
+//     $data = [
+//         'title' => "Page de ".$firstname,
+//         'description' => "Page de ".$firstname .' '. $lastname,
+//         'firstname' => $firstname,
+//         'lastname' => $lastname,
+//     ];
 
-    $count = request()->query('count', 5);
+//     return view('test.index', $data);
+// });
 
-    dump($count);
+/*
+|--------------------------------------------------------------------------
+| ch 4
+|--------------------------------------------------------------------------
+*/
 
-    switch ($sort) {
-        case 'desc':
-            rsort($articles);
-            break;
+// // utiliser les params URL
+// Route::get("test/{user}", function ($user) {
+//     echo 'hello ' . $user;
+// });
 
-        case 'asc':
-            sort($articles);
-            break;
+// // utiliser les params URL
+// // Route::get('profile/{name?}', function ($name = null) {
+// //     if ($name) {
+// //         echo 'hello ' . $name;
+// //     } else {
+// //         echo 'hello stranger';
+// //     }
+// // });
 
-        default:
-            # code...
-            break;
-    }
+// //trier les articles avec les query string
+// Route::get('articles', function () {
+//     $articles = ['B', 'A', 'C'];
 
-    foreach ($articles as $article) {
-        echo '<p>' . $article . '</p>';
-    }
-});
+//     $sort = request()->query('sort', 'desc');
+
+//     $count = request()->query('count', 5);
+
+//     dump($count);
+
+//     switch ($sort) {
+//         case 'desc':
+//             rsort($articles);
+//             break;
+
+//         case 'asc':
+//             sort($articles);
+//             break;
+
+//         default:
+//             # code...
+//             break;
+//     }
+
+//     foreach ($articles as $article) {
+//         echo '<p>' . $article . '</p>';
+//     }
+// });
