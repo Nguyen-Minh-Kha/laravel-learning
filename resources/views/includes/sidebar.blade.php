07b22edc97876bffe880798ed300d7eab0c1e80a
@Nguyen-Minh-Kha <pro.nguyenminhkha@gmail.com>
2022-11-22 16:26:25 +0100

    7.3 include views

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,9 @@
+<div class="my-4">
+    Category
+</div>
+<div class="list-group">
+    <a href="#" class="list-group-item active">Laravel</a>
+    <a href="#" class="list-group-item">PHP</a>
+    <a href="#" class="list-group-item">JS</a>
+    <a href="#" class="list-group-item">VueJS</a>
+</div>
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,14 +4,7 @@
 <div class="row">
 
     <div class="col-lg-3">
-        <div class="my-4">
-            Category
-        </div>
-        <div class="list-group">
-            <a href="#" class="list-group-item active">Laravel</a>
-            <a href="#" class="list-group-item">PHP</a>
-            <a href="#" class="list-group-item">JS</a>
-        </div>
+        @include('includes.sidebar')
     </div>
 
     <div class="col-lg-9">
