1bb7287cf5cb40a0c3029012224f3ab801bd4fc0
Nguyen-Minh-Kha <pro.nguyenminhkha@gmail.com>
2022-11-25 14:36:26 +0100

    9.2 Implémenter des relations belongsTo et hasMany

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    /**
+     * Un article appartien à un user 
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Un user peut avoir plusieurs articles 
+     */
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
 }
