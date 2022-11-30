669483c9d48d05df92a2d25db80336f5c71f0fcf
Nguyen-Minh-Kha <pro.nguyenminhkha@gmail.com>
2022-12-01 00:15:56 +0100

    11.7 send email to reset pwd

--- app/Http/Controllers/ForgotController.php
+++ app/Http/Controllers/ForgotController.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+
+use Illuminate\Support\Str;
+
+use Illuminate\Support\Facades\DB;
+
+use App\Notifications\PasswordResetNotification;
 
 class ForgotController extends Controller
 {
@@ -27,5 +34,21 @@ class ForgotController extends Controller
         request()->validate([
             'email' => 'required|email|exists:users'
         ]);
+
+        $token = Str::uuid();
+
+        DB::table('password_resets')->insert([
+            'email' => request('email'),
+            'token' => $token,
+            'created_at' => now(),
+        ]);
+
+        $user = User::whereEmail(request('email'))->firstOrfail();
+
+        $user->notify(new PasswordResetNotification($token));
+
+        // send notification with a secure link
+        $success = 'Please check your email and follow the instructions';
+        return back()->withSuccess($success);
     }
 }
---
+++ docs/Notifications.md
@@ -0,0 +1,3 @@
+```
+php artisan make:notification <name>
+```
