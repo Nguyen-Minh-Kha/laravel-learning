1f19f8bbef0c7ce5a431740e613dbba4d0d00182
Nguyen-Minh-Kha <pro.nguyenminhkha@gmail.com>
2022-12-01 23:59:24 +0100

    20.3 create routes and fct in controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,8 +7,35 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth')->except('profile');
+    }
+
     public function profile(User $user)
     {
-        return 'I am an user and my name is '.$user->name;
+        return 'I am an user and my name is ' . $user->name;
     }
+
+    /**
+     *  edit user form
+     */
+    public function edit()
+    {
+        $user = auth()->user();
+        $data = [
+            'title' => $description = 'edit my profile',
+            'description' => $description,
+            'user' => $user,
+        ];
+
+        return view('user.edit', $data);
+    }
+
+    /**
+     *  save user infos
+     */
+    public function store()
+    { }
 }
