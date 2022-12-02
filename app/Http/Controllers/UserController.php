997f7dfa79b063a51a22b318bf1bbf2a43c71a2e
Nguyen-Minh-Kha <pro.nguyenminhkha@gmail.com>
2022-12-02 01:05:52 +0100

    20.7 save image

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
@@ -43,7 +46,21 @@ class UserController extends Controller
         request()->validate([
             'name' => ['required', 'min:3', 'max:20', Rule::unique('users')->ignore($user)],
             'email' => ['required', 'email', Rule::unique('users')->ignore($user)],
-            'avatar' => ['sometimes', 'nullable', 'file', 'image', 'mimes:jpg,jpeg,png,gif'],
+            'avatar' => ['sometimes', 'nullable', 'file', 'image', 'mimes:jpg,jpeg,png,gif'], // 'dimensions:min_width=100,min_height=200'
         ]);
+
+        if (request()->hasFile('avatar') && request()->file('avatar')->isValid()) {
+            $ext = request()->file('avatar')->extension();
+            $filename = Str::slug($user->name) . '-' . $user->id . '.' . $ext;
+            $path = request()->file('avatar')->storeAs('avatars/' . $user->id, $filename);
+
+            $thumbnailImage = Image::make(request()->file('avatar'))->fit(200, 200, function ($constraint) {
+                $constraint->upsize();
+            })->encode($ext, 50);
+
+            $thumbnailPath = 'avatars/' . $user->id . '/thumbnail/' . $filename;
+
+            Storage::put($thumbnailPath, $thumbnailImage);
+        }
     }
 }
